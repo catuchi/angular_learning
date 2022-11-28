08e15d5cc6e35230cd5c959587655ec6a7c4c832
Chibu <atuchibueze@gmail.com>
2022-11-28 03:23:15 -0500

    life cycle hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-learning';
+export class AppComponent implements OnInit, OnDestroy {
+  intervalSub;
+
+  ngOnInit() {
+    this.intervalSub = setInterval(() => {
+      console.log('Hello from ngOnInit');
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalSub) {
+      clearInterval(this.intervalSub);
+    }
+  }
 }
