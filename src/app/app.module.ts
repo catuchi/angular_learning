7b8486359e77ca9c2c9a4e05e8082c4c07d16ae5
Chibu <atuchibueze@gmail.com>
2022-11-30 12:58:36 -0500

    ng-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './hello-world.component';
 import { ParentComponent } from './components/parent/parent.component';
 import { ChildComponent } from './components/child/child.component';
+import { ChildTwoComponent } from './components/child-two/child-two.component';
 
 @NgModule({
-  declarations: [AppComponent, HelloWorldComponent, ParentComponent, ChildComponent],
+  declarations: [AppComponent, ParentComponent, ChildComponent, ChildTwoComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
